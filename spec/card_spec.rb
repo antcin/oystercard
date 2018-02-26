81fff23fa7b28af462e5c7fceaf60a3cab6dd93a
Antonio <ciniglioantonio@gmail.com>
2018-02-26 16:11:34 +0000

    Write test for Card#top_up and implement method. Challenge 5 completed

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,8 +1,18 @@
 require 'card'
+
 describe Card do
   describe '#balance' do
     it 'should return a balance of 0' do
       expect(subject.balance).to eq 0
+    end
+  end
+
+  describe '#top_up' do
+    it 'respond to top_up with one argument' do
+      expect(subject).to respond_to(:top_up).with(1).argument
+    end
+    it 'should top up te balance' do
+      expect { subject.top_up(1) }.to change { subject.balance }.by 1
     end
   end
 end
